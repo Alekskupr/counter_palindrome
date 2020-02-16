76a5b5ad1f046b51256e0a2164e8f4fbe04e740f
Alekskupr <leshakuprish@yandex.ru>
2020-02-16 22:10:26 +0300

    решение задания

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: '0',
+      palindrome: false,
+    };
+  }
 
-export default App;
+  counterIncrease = () => {
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => {
+      this.setState({ counter: (+ this.state.counter + 1).toString() });
+    }, 250);
+  };
+
+  palindromeTest = () => {
+    clearTimeout(this.timer);
+    let str = this.state.counter;
+    this.setState({
+      palindrome:
+        str ===
+        str
+          .split('')
+          .reverse()
+          .join(''),
+    });
+    const modal = document.getElementsByClassName('modal')[0];
+    modal.style.display = 'block';
+  };
+
+  closeModal = () => {
+    const modal = document.getElementsByClassName('modal')[0];
+    modal.style.display = 'none';
+  };
+
+  render() {
+    const palindrome = this.state.palindrome;
+    return (
+      <div className="App">
+        <div className="container">
+          <button className="forkButton" onDoubleClick={this.palindromeTest} onClick={this.counterIncrease}></button>
+          <div className="counter">
+            <p class="counterText">{this.state.counter}</p>
+          </div>
+        </div>
+
+        <div className="modal">
+          <div className="modal_content">
+            <span onClick={this.closeModal} className="close">
+              X
+            </span>
+            <p className="answer">{palindrome ? 'Это палиндром!!!' : 'Не, это не палиндром'}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
